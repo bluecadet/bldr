09004fb6eee157b2bf0511e2ee6f27db3ef004c4
Shaun Baer <shaun.baer@gmail.com>
2022-06-03 17:41:11 -0400

    update examples

---
+++ examples/bldrConfig.js
@@ -0,0 +1,103 @@
+const buildStatistics = require('rollup-plugin-build-statistics');
+
+module.exports = {
+  dev: {
+    css: {
+      src: './web/theme/sample_theme/assets/src/css/*.css',
+      dest: './web/theme/sample_theme/assets/dist/css/',
+      watch: [
+        './web/theme/sample_theme/assets/src/**/*.css',
+        './web/theme/sample_theme/components/**/*.css',
+      ]
+    },
+    js: {
+      src: './web/theme/sample_theme/assets/src/js/*.js',
+      dest: './web/theme/sample_theme/assets/dist/js/',
+      watch: [
+        './web/theme/sample_theme/assets/src/**/*.js'
+      ],
+    },
+    image: {
+      src: '',
+      dest: '',
+      // watch: '',
+    },
+    env: {
+      fractal: {
+        css: [
+          {
+            src: './web/theme/sample_theme/assets/src/css/*.css',
+            dest: './web/theme/sample_theme/assets/dist/css/',
+            watch: [
+              './web/theme/sample_theme/assets/src/**/*.css',
+              './web/theme/sample_theme/components/**/*.css',
+            ]
+          },
+          {
+            src: './web/theme/admin_theme/assets/src/css/*.css',
+            dest: './web/theme/admin_theme/assets/dist/css/',
+            watch: [
+              './web/theme/admin_theme/assets/src/**/*.css',
+              './web/theme/admin_theme/components/**/*.css',
+            ]
+          }
+        ],
+        js: [
+          {
+            src: './web/theme/sample_theme/assets/src/js/*.js',
+            dest: './web/theme/sample_theme/assets/dist/js/',
+            watch: [
+              './web/theme/sample_theme/assets/src/js/**/*.js'
+            ]
+          },
+          {
+            src: './web/theme/admin_theme/assets/src/js/*.js',
+            dest: './web/theme/admin_theme/assets/dist/js/',
+            watch: [
+              './web/theme/admin_theme/assets/src/js/**/*.js'
+            ]
+          }
+        ]
+      }
+    }
+  },
+  build: {
+    css: {
+      src: './web/theme/sample_theme/assets/src/css/*.css',
+      dest: './web/theme/sample_theme/assets/dist/css/',
+      watch: [
+        './web/theme/sample_theme/assets/src/**/*.css',
+        './web/theme/sample_theme/components/**/*.css',
+      ]
+    },
+    js: {
+      src: './web/theme/sample_theme/assets/src/js/*.js',
+      dest: './web/theme/sample_theme/assets/dist/js/',
+    },
+    image: {
+      src: '',
+      dest: '',
+      watch: '',
+    }
+  },
+  postCSS: {
+    plugins: [
+      require('postcss-assets')
+    ],
+    overridePlugins: false
+  },
+  esBuild: {
+    plugins: [
+      require("essass"),
+    ]
+  },
+  rollup: {
+    inputOptions: {
+      plugins: [
+        buildStatistics({
+          projectName: 'awesome-project',
+        }),
+      ]
+    }
+  }
+}
---
+++ examples/bldrConfigLocal.js
@@ -0,0 +1,5 @@
+module.exports = {
+  browserSync: {
+    port: 4000
+  }
+}
