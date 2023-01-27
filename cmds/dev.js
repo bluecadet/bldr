6b6878e14bd1d2b709e1456ec039d6d1470b1a28
Shaun Baer <shaun.baer@gmail.com>
2023-01-27 12:57:24 -0500

    update env settings and browsersync error

--- cmds/dev.js
+++ cmds/dev.js
@@ -1,3 +1,4 @@
+const {setEnv} = require('../lib/utils/configHelpers');
 exports.command = 'dev'
 exports.desc = 'watch files and run local development tasks'
 
@@ -6,6 +7,7 @@ exports.builder = (yargs) => {
 }
 
 exports.handler = function (args) {
+  setEnv('dev');
   let RunDev = require('../lib/dev/runDev');
   let runDev = new RunDev(args);
   runDev.run();
--- cmds/watch.js
+++ cmds/watch.js
@@ -1,3 +1,4 @@
+const {setEnv} = require('../lib/utils/configHelpers');
 exports.command = 'watch'
 exports.desc = 'watch files and run local development tasks'
 
@@ -6,6 +7,7 @@ exports.builder = (yargs) => {
 }
 
 exports.handler = function (args) {
+  setEnv('dev');
   let RunWatch = require('../lib/watch/runWatch.js');
   let runWatch = new RunWatch(args);
   runWatch.run();
