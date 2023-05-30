1bf8eef01ac81ef5e8d264ae7e5c1ee6dd3cce13
Shaun Baer <shaun.baer@gmail.com>
2023-05-30 14:01:57 -0400

    v1 release

--- commands/dev.js
+++ commands/dev.js
@@ -70,7 +70,7 @@ export const RunBldrDev = async (commandOptions) => {
       await processSass(configData);
       if ( bsInstance ) {
         bsInstance.stream({match: "**/*.css"});
-        bsInstance.reload("*.css");
+        bsInstance.reload("**/*.css");
       }
     }
 
@@ -78,21 +78,21 @@ export const RunBldrDev = async (commandOptions) => {
       await processPostcss(configData);
       if ( bsInstance ) {
         bsInstance.stream({match: "**/*.css"});
-        bsInstance.reload("*.css");
+        bsInstance.reload("**/*.css");
       }
     }
 
     if (jsExts.includes(ext)) {
       await processEsBuild(configData);
       if ( bsInstance ) {
-        bsInstance.reload();
+        bsInstance.reload({stream: true});
       }
     }
 
     if (imageExts.includes(ext)) {
       await processImages(configData);
       if ( bsInstance ) {
-        bsInstance.reload();
+        bsInstance.reload({stream: true});
       }
     }
 
@@ -161,190 +161,3 @@ export const RunBldrDev = async (commandOptions) => {
   }
 
 }
-
-
-
-
-
-  /**
-   * Run esbuild
-   * @param {object} bsInstance BrowserSync Instance
-   */
-  // this.esBuild = (bsInstance = false) => {
-  //   handleEsBuild(
-  //     configData,
-  //     configRootData?.esbuild,
-  //     bsInstance,
-  //   );
-  // }
-
-  // /**
-  //  * Run PostCSS function
-  //  * @param {object} bsInstance Browsersync Instance
-  //  */
-  // this.handleWatchPostCSS = (bsInstance = false) => {
-
-  //   handlePostCss(
-  //     configData,
-  //     configRootData?.postCSS,
-  //     false,
-  //     bsInstance
-  //   )
-  // }
-
-
-  // /**
-  //  * Run PostCSS function
-  //  * @param {object} bsInstance Browsersync Instance
-  //  */
-  // this.handleWatchSass = (bsInstance = false) => {
-
-  //   handleSass(
-  //     configData,
-  //     false,
-  //     bsInstance
-  //   )
-  // }
-
-  // /**
-  //  * Run Images function
-  //  * @param {object} bsInstance Browsersync Instance
-  //  */
-  // this.handleWatchImages = (bsInstance = false) => {
-  //   runImages(
-  //     configData
-  //   );
-  // }
-
-  // this.esBuild = (bsInstance = false) => {
-  //   handleEsBuild(
-  //     configData,
-  //     configRootData?.esbuild,
-  //     bsInstance,
-  //   );
-  // }
-
-
-
-
-
-  // if ( configData?.watchReload ) {
-  //   configData.watchReload.forEach(p => {
-  //     this.reloadExts.push(path.extname(p))
-  //   });
-  // }
-
-
-  // this.handleFileUpdates = ( type, bsInstance, filepath ) => {
-
-  //   if (this.sassExts.includes(type)) {
-  //     this.handleWatchSass(bsInstance);
-  //   }
-
-  //   if (this.postCssExts.includes(type)) {
-  //     this.handleWatchPostCSS(bsInstance);
-  //   }
-
-  //   if (this.jsExts.includes(type)) {
-  //     this.esBuild(bsInstance);
-  //   }
-
-  //   if (this.imageExts.includes(type)) {
-  //     this.handleWatchImages(bsInstance);
-  //   }
-
-  //   if (this.reloadExts.includes(type)) {
-  //     bsInstance.reload();
-  //     handleMessageSuccess('bldr', `${path.basename(filepath)} triggered reload`);
-  //   }
-  // }
-
-  // this.handleChokidar = (watchPaths, bs) => {
-  //   const watcher = chokidar.watch(watchPaths.flat(), {
-  //     ignored: /(^|[\/\\])\../, // ignore dotfiles
-  //     persistent: true,
-  //     ignoreInitial: true,
-  //   });
-
-  //   watcher
-  //     .on('ready', () => {
-  //       console.log(``);
-  //       console.log(`----------------------------------------`);
-  //       console.log(`[${colors.blue('bldr')}] ${colors.magenta('💪 Ready and waiting for changes!')}`)
-  //       console.log(`----------------------------------------`);
-  //       console.log(``);
-  //     })
-  //     .on('add', filePath => {
-  //       this.handleFileUpdates(path.extname(filePath), bs, filePath);
-  //     })
-  //     .on('change', filePath => {
-  //       this.handleFileUpdates(path.extname(filePath), bs, filePath);
-  //     })
-  //     .on('unlink', filePath => {
-  //       this.handleFileUpdates(path.extname(filePath), bs, filePath);
-  //     })
-  // }
-
-
-  // this.run = () => {
-
-  //   if ( envKey ) {
-  //     console.log(`Starting dev using ${envKey} enviornment...`.green);
-  //   } else {
-  //     console.log('Starting dev...'.green);
-  //   }
-
-  //   if ( !configData?.disableBrowsersync ) {
-  //     if ( !localConfigData?.browserSync ) {
-  //       handleMessageWarn('bldr', 'Local browserSync config not setup. Create a bldrConfigLocal.js.js file in root or run `bldr init`');
-  //     } else if ( !localConfigData?.browserSync?.proxy ) {
-  //       handleMessageWarn('bldr', 'Local browserSync proxy not setup. Add a `proxy` key within `browserSync` with a value for your local domain for dependancy injection.');
-  //     }
-  //   }
-
-  //   const watchPaths = [];
-
-  //   if (configData?.css) {
-  //     watchPaths.push(this.createWatchPathArray(configData.css));
-  //   }
-
-  //   if (configData?.sass) {
-  //     watchPaths.push(this.createWatchPathArray(configData.sass));
-  //   }
-
-  //   if (configData?.js) {
-  //     watchPaths.push(this.createWatchPathArray(configData.js));
-  //   }
-
-  //   if (configData?.images) {
-  //     watchPaths.push(this.createWatchPathArray(configData.images));
-  //   }
-
-  //   if (configData?.watchReload) {
-  //     watchPaths.push(configData.watchReload);
-  //   }
-
-  //   if ( watchPaths.length <= 0 ) {
-  //     throw new Error('Watch paths are not defined in bldrConfig.js.'.red);
-  //   }
-
-
-  //   // INIT BROWSERSYNC
-  //   const bsDisable = configData?.browsersync?.disable ? configData.browsersync.disable : false;
-
-  //   if ( bsDisable ) {
-  //     this.handleChokidar(watchPaths, false);
-  //   } else {
-  //     const bsOptions = localConfigData?.browserSync ?? {};
-  //     const bsName    = localConfigData?.browserSync?.instanceName ?? `bldr-${Math.floor(Math.random() * 1000)}`;
-  //     const bs        = require("browser-sync").create(bsName);
-
-  //     bsOptions.logPrefix      = 'bldr';
-  //     bsOptions.logFileChanges = false;
-
-  //     bs.init(bsOptions, this.handleChokidar(watchPaths, bs));
-  //   }
-
-
-  // }
-// }
