ae913bcb6d27c9abc45b2315d042aa6c3829d146
Shaun Baer <shaun.baer@gmail.com>
2024-05-22 13:23:11 -0400

    start class transition

--- commands/build.js
+++ commands/build.js
@@ -4,16 +4,22 @@ import { processSass } from '../lib/processes/sass.js';
 import { processPostcss } from '../lib/processes/postcss.js';
 import { processRollup } from '../lib/processes/rollup.js';
 import { processImages } from '../lib/processes/images.js';
+import { Bldr_Config } from '../lib/Bldr_Config.js';
+import { Processor__SDC } from '../lib/Processor__SDC.js';
 
 export const RunBldrBuild = async (commandOptions) => {
-
-  const configData  = await getConfigData(commandOptions);
-  const envKey      = commandOptions.settings?.key;
+  const configData = await getConfigData(commandOptions);
+  const BldrConfig = new Bldr_Config();
+  const envKey = commandOptions.settings?.key;
+  let SDC = false;
 
   console.log(``);
   console.log(`----------------------------------------`);
-  if ( envKey ) {
-    handleProcessAction('bldr', '💪 Starting build using ${envKey} env configuration...');
+  if (envKey) {
+    handleProcessAction(
+      'bldr',
+      '💪 Starting build using ${envKey} env configuration...'
+    );
   } else {
     handleProcessAction('bldr', '💪 Starting build...');
   }
@@ -21,6 +27,14 @@ export const RunBldrBuild = async (commandOptions) => {
   console.log(``);
 
   const processStart = new Date().getTime();
+
+  if (BldrConfig.isSDC) {
+    SDC = new Processor__SDC();
+    SDC.init();
+    await SDC.processFiles();
+  }
+
+  return;
 
   await Promise.all([
     processSass(configData),
@@ -31,7 +45,9 @@ export const RunBldrBuild = async (commandOptions) => {
 
   const processEnd = new Date().getTime();
 
-
-  handleProcessAction('bldr', '✨ Build complete ✨', `${(processEnd - processStart)/1000}`);
-
-}
+  handleProcessAction(
+    'bldr',
+    '✨ Build complete ✨',
+    `${(processEnd - processStart) / 1000}`
+  );
+};
--- commands/dev.js
+++ commands/dev.js
@@ -1,5 +1,9 @@
 import { getConfigData } from '../lib/utils/getConfigData.js';
-import { handleProcessAction, handleProcessWarn, handleProcessSuccess } from '../lib/utils/reporters.js';
+import {
+  handleProcessAction,
+  handleProcessWarn,
+  handleProcessSuccess,
+} from '../lib/utils/reporters.js';
 import { settings } from '../lib/settings/bldrSettings.js';
 import { processSass } from '../lib/processes/sass.js';
 import { processPostcss } from '../lib/processes/postcss.js';
@@ -8,26 +12,37 @@ import { processImages } from '../lib/processes/images.js';
 import { basename, extname } from 'node:path';
 
 // import util from 'node:util';
-import { createRequire } from 'node:module'
+import { createRequire } from 'node:module';
+import { Processor__SDC } from '../lib/Processor__SDC.js';
+
 const require = createRequire(import.meta.url);
 const chokidar = require('chokidar');
 
-
 export const RunBldrDev = async (commandOptions) => {
+  const configData = await getConfigData(commandOptions);
 
-  const configData  = await getConfigData(commandOptions);
-  const envKey      = commandOptions.settings?.key;
-  const sassExts    = ['.scss', '.sass'];
-  const postCssExts = ['.css','.pcss','.postcss'];
-  const jsExts      = ['.js','.jsx', '.cjs', '.mjs'];
-  const imageExts   = ['.jpg','.jpeg','.png','.gif','.svg', 'webp'];
-  let   bsInstance  = false;
-  let   bsInstanceName = false;
+  const envKey = commandOptions.settings?.key;
+  const sassExts = ['.scss', '.sass'];
+  const postCssExts = ['.css', '.pcss', '.postcss'];
+  const jsExts = ['.js', '.jsx', '.cjs', '.mjs'];
+  const imageExts = ['.jpg', '.jpeg', '.png', '.gif', '.svg', 'webp'];
+  let bsInstance = false;
+  let bsInstanceName = false;
+  const isSDC = configData?.processes?.singleDirectoryComponent;
+  let SDC = false;
 
-  if ( commandOptions.settings?.once ) {
+  if (isSDC) {
+    SDC = new Processor__SDC();
+    SDC.init();
+    handleProcessAction('bldr', 'Single Directory Component structure active');
+  }
 
-    if ( envKey ) {
-      handleProcessAction('bldr', `Running single dev build using ${envKey} env configuration...`);
+  if (commandOptions.settings?.once) {
+    if (envKey) {
+      handleProcessAction(
+        'bldr',
+        `Running single dev build using ${envKey} env configuration...`
+      );
     } else {
       handleProcessAction('bldr', 'Running single dev build...');
     }
@@ -39,71 +54,105 @@ export const RunBldrDev = async (commandOptions) => {
     await processEsBuild(configData);
     await processImages(configData);
 
+    if (isSDC) {
+      SDC.processFiles();
+    }
+
     const processEnd = new Date().getTime();
 
-    handleProcessAction('bldr', '✨ Dev processes complete ✨', `${(processEnd - processStart)/1000}`);
+    handleProcessAction(
+      'bldr',
+      '✨ Dev processes complete ✨',
+      `${(processEnd - processStart) / 1000}`
+    );
 
     return;
   }
 
-
-  if ( envKey ) {
-    handleProcessAction('bldr', `Starting dev using ${envKey} env configuration...`);
+  if (envKey) {
+    handleProcessAction(
+      'bldr',
+      `Starting dev using ${envKey} env configuration...`
+    );
   } else {
     handleProcessAction('bldr', 'Starting dev...');
   }
 
   // Run processes before starting local build
-  if ( commandOptions.settings?.start ) {
+  if (commandOptions.settings?.start) {
     handleProcessAction('bldr', 'Running initial processes...');
     await processSass(configData, bsInstance);
     await processPostcss(configData, bsInstance);
     await processEsBuild(configData, bsInstance);
     await processImages(configData, bsInstance);
-  }
 
+    if (isSDC) {
+      SDC.processFiles();
+    }
+  }
 
   // Handle a single file from Chokidar event
   const handleFile = async (ext, file) => {
 
-    if (sassExts.includes(ext)) {
-      await processSass(configData);
-      if ( bsInstance ) {
-        // bsInstance.stream({match: "**/*.css"});
-        bsInstance.reload(["*.css"]);
-      }
-    }
+    if (isSDC && SDC.isFile(file, ext)) {
+      await SDC.processFile(file);
 
-    if (postCssExts.includes(ext)) {
-      await processPostcss(configData);
-      if ( bsInstance ) {
-        // bsInstance.stream({match: "**/*.css"});
-        bsInstance.reload(["*.css"]);
-      }
-    }
+      if (bsInstance) {
+        if (sassExts.includes(ext) || postCssExts.includes(ext)) {
+          // bsInstance.stream({match: "**/*.css"});
+          bsInstance.reload(['*.css']);
+        }
 
-    if (jsExts.includes(ext)) {
-      await processEsBuild(configData);
-      if ( bsInstance ) {
-        bsInstance.reload(["*.js"]);
+        if (jsExts.includes(ext)) {
+          bsInstance.reload(['*.js']);
+        }
       }
-    }
+    } else {
+      if (sassExts.includes(ext)) {
+        await processSass(configData);
 
-    if (imageExts.includes(ext)) {
-      await processImages(configData);
-      if ( bsInstance ) {
+        if (bsInstance) {
+          // bsInstance.stream({match: "**/*.css"});
+          bsInstance.reload(['*.css']);
+        }
+      }
+
+      if (postCssExts.includes(ext)) {
+        await processPostcss(configData);
+
+        if (bsInstance) {
+          // bsInstance.stream({match: "**/*.css"});
+          bsInstance.reload(['*.css']);
+        }
+      }
+
+      if (jsExts.includes(ext)) {
+        if (isSDC && SDC.isFile(file, 'sass')) {
+          await SDC.processFile(file);
+        } else {
+          await processEsBuild(configData);
+        }
+
+        if (bsInstance) {
+          bsInstance.reload(['*.js']);
+        }
+      }
+
+      if (imageExts.includes(ext)) {
+        await processImages(configData);
+        if (bsInstance) {
+          bsInstance.reload();
+        }
+      }
+
+      if (configData.watch.reloadExts.includes(ext) && bsInstance) {
         bsInstance.reload();
+        handleProcessSuccess('bldr', `${basename(file)} triggered reload`);
       }
-    }
-
-    if (configData.watch.reloadExts.includes(ext) && bsInstance ) {
-      bsInstance.reload();
-      handleProcessSuccess('bldr', `${basename(file)} triggered reload`);
     }
   };
 
   const handleChokidar = () => {
-
     // Chokidar watcher
     const watcher = chokidar.watch(configData.watch.files, {
       ignored: /(^|[\/\\])\../, // ignore dotfiles
@@ -119,29 +168,34 @@ export const RunBldrDev = async (commandOptions) => {
         console.log(`-------------------------------------------`);
         console.log(``);
       })
-      .on('add', filePath => {
+      .on('add', (filePath) => {
         handleFile(extname(filePath), filePath);
       })
-      .on('change', filePath => {
+      .on('change', (filePath) => {
         handleFile(extname(filePath), filePath);
       })
-      .on('unlink', filePath => {
+      .on('unlink', (filePath) => {
         handleFile(extname(filePath), filePath);
       });
-  }
-
-
+  };
 
   // Create browsersync instance if appropriate
-  if ( !configData?.processSettings?.browsersync?.disable ) {
-    if ( !configData?.local ) {
-      handleProcessWarn('bldr', `Create a ${settings.localFilename} file in project root to configure browsersync`);
+  if (!configData?.processSettings?.browsersync?.disable) {
+    if (!configData?.local) {
+      handleProcessWarn(
+        'bldr',
+        `Create a ${settings.localFilename} file in project root to configure browsersync`
+      );
     }
 
     const bsLocalOpts = configData.local || {};
-    const bsOptions   = bsLocalOpts?.browserSync ? {...bsLocalOpts.browserSync} : {};
-    bsInstanceName    = bsLocalOpts?.browserSync?.instanceName ?? `bldr-${Math.floor(Math.random() * 1000)}`;
-    bsInstance        = await require("browser-sync").create(bsInstanceName);
+    const bsOptions = bsLocalOpts?.browserSync
+      ? { ...bsLocalOpts.browserSync }
+      : {};
+    bsInstanceName =
+      bsLocalOpts?.browserSync?.instanceName ??
+      `bldr-${Math.floor(Math.random() * 1000)}`;
+    bsInstance = await require('browser-sync').create(bsInstanceName);
 
     // Bldr enforced options
     bsOptions.logPrefix = 'bldr';
@@ -150,14 +204,11 @@ export const RunBldrDev = async (commandOptions) => {
     bsInstance.init(bsOptions, () => {
       handleChokidar();
     });
-
   } else {
-
-    if ( configData?.processSettings?.browsersync?.disable ) {
+    if (configData?.processSettings?.browsersync?.disable) {
       handleProcessWarn('bldr', 'Browsersync is disabled in config');
     }
 
     handleChokidar();
   }
-
-}
+};
