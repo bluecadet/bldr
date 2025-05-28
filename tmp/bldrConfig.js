c43d22961d681edb44c6a14f76537cd605d1ed58
Shaun M Baer <shaun.baer@gmail.com>
2025-05-28 09:54:34 -0400

    cleanup, add npmignore

--- tmp/bldrConfig.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bldrConfig = bldrConfig;
-function bldrConfig(config) {
-    return config;
-}
-//# sourceMappingURL=bldrConfig.js.map
